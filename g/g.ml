413581a97fd91c32380d4bfb85caf124d34a5d22
Timothy Zhu <tz277@cornell.edu>
2025-08-07 02:46:16 -0400

    compiles but doesn't lint

--- g/g.ml
+++ g/g.ml
@@ -1,1 +1,8 @@
 let () = print_endline "Hello, G!"
+let y = Gsystem.Hello.world;;
+
+print_endline y;;
+print_endline y;;
+print_endline y
+
+(* dune exec g *)
---
+++ g/gsystem.ml
@@ -0,0 +1,3 @@
+module Hello = struct
+  let world = "WORLD"
+end
